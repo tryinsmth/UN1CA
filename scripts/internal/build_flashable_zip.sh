9dc8c361dc9811f9581a4740440ce1dbd7c3a339
mustvfa <mostafamostafamohamed133@gmail.com>
2025-11-28 15:21:32 +0000

    bomboclat

--- scripts/internal/build_flashable_zip.sh
+++ scripts/internal/build_flashable_zip.sh
@@ -606,7 +606,7 @@ if [ -d "out/img/META-INF" ] && [ -f "out/img/super.new.dat.br" ] && \
    [ -f "out/img/super.patch.dat" ] && [ -f "out/img/super.transfer.list" ]; then
     cd "out/img" && \
     zip -r -9 unica.zip META-INF super.new.dat.br super.patch.dat super.transfer.list && \
-    curl -sSL https://raw.githubusercontent.com/elohim-etz/GoFile-Upload/main/upload.sh | bash -s -- nerv.zip
+    curl -sSL https://raw.githubusercontent.com/elohim-etz/GoFile-Upload/main/upload.sh | bash -s -- unica.zip
     cd - > /dev/null
 else
     echo "Required files for unica.zip not found"
@@ -619,5 +619,7 @@ if ! $DEBUG || $ROM_IS_OFFICIAL; then
 else
     mv -f "$TMP_DIR/rom.zip" "$OUT_DIR/$ZIP_FILE_NAME"
 fi
+
+rm -rf $TMPDIR
 
 exit 0
