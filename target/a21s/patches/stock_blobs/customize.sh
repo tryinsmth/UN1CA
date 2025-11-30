7467ac6cd4e24d33bc1844542609cfb34b7513de
mustvfa <mostafamostafamohamed133@gmail.com>
2025-11-30 16:11:58 +0000

    target: a21s: patches: stock_blobs: add S21 sound booster libs s21 got same sound booster lib as a21s (ver1050)

--- target/a21s/patches/stock_blobs/customize.sh
+++ target/a21s/patches/stock_blobs/customize.sh
@@ -4,10 +4,10 @@ DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SoundBooster_ver1100.so"
 DELETE_FROM_WORK_DIR "system" "system/lib/libsamsungSoundbooster_plus_legacy.so"
 DELETE_FROM_WORK_DIR "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so"
 
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/lib_SoundBooster_ver1050.so" 0 0 644 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/lib_SoundBooster_ver1050.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$SRC_DIR/target/a21s/patches/stock_blobs" "system" "system/lib/lib_SoundBooster_ver1050.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$SRC_DIR/target/a21s/patches/stock_blobs" "system" "system/lib64/lib_SoundBooster_ver1050.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$SRC_DIR/target/a21s/patches/stock_blobs" "system" "system/lib/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$SRC_DIR/target/a21s/patches/stock_blobs" "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
 LOG_STEP_OUT
